6f52feff2727690802d0911dd0fb7e12cd3f9178
DuatTran268 <duatsen36@gmail.com>
2022-04-26 22:03:39 +0700

    discover header title

--- react/src/pages/KhamPhaKieuToc.js
+++ react/src/pages/KhamPhaKieuToc.js
@@ -1,9 +1,42 @@
 import React from "react";
+import "../styles/khamphakieutoc.css";
 
 const KhamPhaKieuToc = () => {
-    return (
-        <h1>Khám phá kiểu tóc</h1>
-    );
+  return (
+    <>
+      <div className="discover">
+        <div className="container">
+        {/* header */}
+          <div className="discover__header">
+            <div className="discover__header-btn">
+              <div className="discover__button">
+                <div className="discover__col">
+                  <button type="button" class="discover_btn--active discover_btn ">
+                    Discover
+                  </button>
+                </div>
+                <div className="discover__col">
+                  <button type="button" class="discover_btn">
+                    Style Master
+                  </button>
+                </div>
+                <div className="discover__col">
+                  <button type="button" class="discover_btn">
+                    Lookbook
+                  </button>
+                </div>
+                
+              </div>
+            </div>
+            <div className="discover__title">
+            CÙNG 30SHINE KHÁM PHÁ <span>XU HƯỚNG TÓC HOT NHẤT</span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default KhamPhaKieuToc;
